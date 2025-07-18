bdf4e5816a20ea8378a855a48ab2af9e4e2e16c5
MaialenTolosa <s_tolosa24@stud.hwr-berlin.de>
2025-07-18 15:46:33 +0200

    Exercise3

--- mapper.py
+++ mapper.py
@@ -33,7 +33,8 @@ for line in sys.stdin:
     # strip removes whitespaces and new lines at the beginning and end of the line
     # the result is a tuple with 6 elements
     data = line.strip().split("\t")
-
+    if len(data) != 6:
+        raise ValueError(f"Invalid line format: {line}")
     # store the 6 elements of the tuple in seperate variables
     date, time, item, category, sales, payment = data
 
